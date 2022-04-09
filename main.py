9a821a7657e110b8a174e88f23abb1e567a683ce
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-08 23:19:07 -0600

    defining report function

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-# TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-user_input = input("What would you like? (espresso/latte/cappuccino): ")
-# TODO 2. Turn off the Coffee Machine by entering “off” to the prompt.
-if user_input == "off":
-    import os
-    os.system('cls||clear')
-    print("Please come again soon.")
-    print("Goodbye!")
+stopped = False
+while not stopped:
+    # TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+    user_input = str(input("What would you like? (espresso/latte/cappuccino): "))
+    # TODO 2. Turn off the Coffee Machine by entering “off” to the prompt.
+    if user_input == "off":
+        print("Please come again soon.")
+        print("Goodbye!")
 
 # TODO 3. Print report.
-
+def report (water, milk, coffee, money):
+    print(f"Water: {water}ml")
+    print(f"Milk: {milk}")
 # TODO 4. Check resources sufficient?
 
 # TODO 5. Process coins.
+coins = {
+    ".25": "quarter",
+    ".10": "dime",
+    ".05": "nickel",
+}
+
 
 # TODO 6. Check transaction successful?
 

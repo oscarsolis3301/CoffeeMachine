3c3287c8bdfed9bded3683676d221641d6feaab5
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-09 21:53:19 -0600

    Functions for each instruction

---
+++ data.py
@@ -0,0 +1,45 @@
+# coffee = [
+#     {"type": "espresso",
+#      "water": 50,
+#      "milk": 0,
+#      "coffee": 18,
+#      "cost": 1.5
+#      }, {
+#         "type": "latte",
+#         "water": 200,
+#         "milk": 150,
+#         "coffee": 24,
+#         "cost": 2.5
+#     }, {
+#         "type": "cappuccino",
+#         "water": 50,
+#         "milk": 0,
+#         "coffee": 18,
+#         "cost": 1.5
+#     }]
+
+coffee = {"espresso": {
+    "water": 50,
+    "milk": 0,
+    "coffee": 18,
+    "cost": 1.5
+}, "latte": {
+    "water": 200,
+    "milk": 150,
+    "coffee": 24,
+    "cost": 2.5
+},
+    "cappuccino": {
+    "water": 50,
+    "milk": 0,
+    "coffee": 18,
+    "cost": 1.5
+    },
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from data import resources, coffee
+
+
 def report(remaining_water, remaining_milk, remaining_coffee, money_total):
-    print(f"Water: {water}ml")
-    print(f"Milk: {milk}ml")
-    print(f"Coffee: {coffee}g")
-    print(f"Money: ${money}")
+    print(f"Water: {remaining_water}ml")
+    print(f"Milk: {remaining_milk}ml")
+    print(f"Coffee: {remaining_coffee}g")
+    print(f"Money: ${money_total}")
 
 
-def check_coins(choice, price):
+def check_balance(choice, price):
     user_quarters = .25 * float(input(f"How many quarters?: "))
     user_dimes = .1 * float(input(f"How many dimes?: "))
     user_nickles = .05 * float(input(f"How many nickles?: "))
@@ -15,16 +18,39 @@ def check_coins(choice, price):
         print("Sorry that's not enough money. Money refunded.")
         return False
     elif user_total >= price:
-        change = round(user_total - price, 2)
-        print(f"Here is ${change} in change.")
-        print(f"Here is your {choice} ☕.Enjoy!")
+        make_coffee(user_total, choice, price)
         return True
 
 
-water = 300
-milk = 200
-coffee = 100
-money = 0
+def make_coffee(user_money, choice, price):
+    change = round(user_money - price, 2)
+    print(f"Here is ${change} in change.")
+    print(f"Here is your {choice} ☕.Enjoy!")
+
+
+def check_resources(choice):
+    if choice in coffee:
+        if coffee[choice]["water"] <= resources["water"]:
+            if coffee[choice]["milk"] <= resources["milk"]:
+                if coffee[choice]["coffee"] <= resources["coffee"]:
+                    update_report(choice)
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+
+def update_report(choice):
+    resources["water"] -= coffee[choice]["water"]
+    resources["milk"] -= coffee[choice]["milk"]
+    resources["coffee"] -= coffee[choice]["coffee"]
+    resources["money"] += coffee[choice]["cost"]
+
 
 stopped = False
 while not stopped:
@@ -36,24 +62,33 @@ while not stopped:
         print("Goodbye!")
         stopped = True
     elif coffee_choice == "report":
-        report(water, milk, coffee, money)
+        report(resources["water"], resources["milk"], resources["coffee"], resources["money"])
     elif coffee_choice == "espresso":
-        enough_balance = check_coins(coffee_choice, price=1.50)
+        enough_resources = check_resources(coffee_choice)
+        if enough_resources:
+            enough_balance = check_balance(coffee_choice, price=1.50)
+        else:
+            print(f"Not enough resources to make a {coffee_choice}")
     elif coffee_choice == "latte":
-        enough_balance = check_coins(coffee_choice, price=2.50)
+        enough_resources = check_resources(coffee_choice)
+        if enough_resources:
+            enough_balance = check_balance(coffee_choice, price=2.50)
+        else:
+            print(f"Not enough resources to make a {coffee_choice}")
     elif coffee_choice == "cappuccino":
-        enough_balance = check_coins(coffee_choice, price=3.00)
+        enough_resources = check_resources(coffee_choice)
+        if enough_resources:
+            enough_balance = check_balance(coffee_choice, price=3.00)
+        else:
+            print(f"Not enough resources to make {coffee_choice}")
+    else:
+        coffee_choice = ""
+        print("Please enter a proper input.")
 # TODO 3. Print report.
 
 # TODO 4. Check resources sufficient?
 
 # TODO 5. Process coins.
-coins = {
-    ".25": "quarter",
-    ".10": "dime",
-    ".05": "nickel",
-    ".01": "pennies",
-}
 
 # TODO 6. Check transaction successful?
 
